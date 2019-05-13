086513c6944eb49d172962ab648b08921ff37747
Gamerx00 <39800344+Gamerx00@users.noreply.github.com>
2019-05-13 13:37:52 -0300

    progrma dos dados - primeira versão

---
+++ Untitled-2.py
@@ -0,0 +1,9 @@
+# meu primeiro programa
+
+from random import randint # importa a função de gerar numeros aleatórios
+
+print("olá 1info2!")
+
+dado1 = randint(1, 6) #gera um numero aleatorio entre 1 e 6
+
+print("Dado1: ",dado1)
